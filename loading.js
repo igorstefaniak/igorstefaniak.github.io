e155e8dde9705dfe3f12d4b287dc94a43491812c
Igor Stefaniak <52112899+igorstefaniak@users.noreply.github.com>
2021-01-28 22:23:30 +0100

    Add files via upload

---
+++ loading.js
@@ -0,0 +1,7 @@
+document.getElementById("container").onload = setTimeout(function() {
+    $('.container:hidden').fadeIn('slow')
+  }, 1200)
+
+document.getElementById("container").onload = setTimeout(function() {
+    $('.loading').fadeOut('slow')
+  }, 1900)
