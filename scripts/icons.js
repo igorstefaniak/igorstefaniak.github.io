653af872328ff24df24c8b9b95d6031fad27cb05
Igor Stefaniak <konta.istefaniak@protonmail.com>
2023-07-10 19:24:07 +0200

    ikony na stopce +

--- scripts/icons.js
+++ scripts/icons.js
@@ -3,6 +3,11 @@ let github = document.querySelector('#github-icon-button');
 let folder = document.querySelector('#folder-icon-button');
 let home = document.querySelector('#home-icon-button');
 
+let twitter = document.querySelector('#twitter-icon-button');
+let facebook = document.querySelector('#facebook-icon-button');
+let instagram = document.querySelector('#instagram-icon-button');
+let youtube = document.querySelector('#youtube-icon-button');
+
 function a (cn) {
 
     let name = String(cn.id).substring(0, String(cn.id).indexOf("-"))
@@ -33,4 +38,9 @@ a(github)
 a(settings)
 a(folder)
 a(home)
+
+a(facebook)
+a(instagram)
+a(youtube)
+a(twitter)
 /* Object.keys({cn})[0] */
--- scripts/loading.js
+++ scripts/loading.js
@@ -3,7 +3,13 @@ $("#container").ready(function(){
         $("#splash").css("opacity", "0");
         $('#container').css( "display", "block" );
     }, 500);
+/*      setTimeout( function() {
+        $("body").css("overflow", "auto");
+    }, 800); */
     setTimeout( function() {
         $("#splash").css("display", "none");
+/*         var styleElement = document.createElement("style");
+        styleElement.appendChild(document.createTextNode("::-webkit-scrollbar {width: 8px!important;} ::-webkit-scrollbar-thumb {background: var(--accent);border-radius: 8px;width: 2px;} ::-webkit-scrollbar-track {background: #1d1c21;}"));
+        document.getElementsByTagName("head")[0].appendChild(styleElement);	 */
     }, 850);
 });
