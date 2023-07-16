833c5a4d7eb963d5f6848b856036d9b6f59c24f8
Igor Stefaniak <konta.istefaniak@protonmail.com>
2023-07-16 22:35:11 +0200

    animacje+ - zmieniono background (jeszcze jeden, jeszcze raz), - dodano animacje do nagłówka/nagłówek - dodano ukrytą animcję - usunięto -webkit-tap-highlight-color na mobilnych - i nie zoptymalizowano żadnego kodu XD

--- scripts/scrollable.js
+++ scripts/scrollable.js
@@ -19,17 +19,27 @@ window.addEventListener('scroll', event => {
 const handleScroll = (event) => {
     const scrollPosition = event.target.scrollingElement.scrollTop;
   
-    console.log("handleScroll");
-  
     const images = document.querySelectorAll("background img");
   
-    console.log("images", images);
-  
+    
     images.forEach((element) => {
-      element.style.transform = `translate(0, ${scrollPosition / 10}px)`;
+      console.log(scrollPosition)
+      if (element.getAttribute("src") == "./images/background/circle_quarter.svg"){
+        element.style.transform = `translate(0, ${scrollPosition / 10}px) rotate(${- scrollPosition / 10}deg) `;
+      }
+      else if (element.getAttribute("src") == "./images/background/turned_pin.svg"){
+        element.style.transform = `translate(0, ${scrollPosition / 10}px) rotate(${- scrollPosition / 10}deg) `;
+      }
+      else if (element.getAttribute("src") == "./images/background/sparkle.svg"){
+        element.style.transform = `translate(0, ${scrollPosition / 10}px) scale(${ scrollPosition / 1000 + 1}) `;
+      }
+      else{
+        element.style.transform = `translate(0, ${scrollPosition / 10}px)`;
+      }
       /* rotate(${scrollPosition / 10}deg) */
     });
   };
   
   window.addEventListener("scroll", handleScroll);
+
   
--- scripts/welcome.js
+++ scripts/welcome.js
@@ -24,3 +24,82 @@ function love(){
     let heart = document.getElementById('heart');
     heart.style.color = "#cc0000";
 }
+
+const btn = document.querySelector("#name");
+var colors = ["#81CAA7", "#D46B84", "#88ABEE", "#C0ADF6"];
+var shapes = ["oval", "triangle", "parallelogram"];
+
+const createParticle = () => {
+  let particle = document.createElement("div");
+  particle.setAttribute("class", "particle");
+
+  let color = colors[Math.floor(Math.random() * colors.length)];
+  let shape = shapes[Math.floor(Math.random() * shapes.length)];
+
+  if (shape === "triangle") {
+    shape = "polygon(50% 0%, 10% 92%, 97% 92%)";
+  } else if (shape === "parallelogram") {
+    shape = "polygon(25% 0%, 73% 0, 43% 98%, 0% 100%)";
+  } else if (shape === "oval") {
+    shape = "ellipse(25% 40% at 50% 50%)";
+  } else if (shape === "star") {
+    shape =
+      "polygon(50% 0%, 61% 35%, 98% 35%, 68% 57%, 79% 91%, 50% 70%, 21% 91%, 32% 57%, 2% 35%, 39% 35%)";
+  }
+
+  // set styles
+  let particleCss = {
+    background: color,
+    clipPath: shape
+  };
+
+  // apply computed styles to particle
+  for (i in particleCss) {
+    particle.style[i] = particleCss[i];
+  }
+
+  let directionX = Math.random() * (200 - -200) - 200;
+  let directionY = Math.random() * (120 - -120) - 120;
+  let rotateDirection = Math.random() * (720 - -720) - 720;
+
+  document.querySelector(".particles").appendChild(particle);
+  gsap.to(particle, {
+    x: directionX,
+    y: directionY,
+    autoAlpha: 0.8,
+    ease: "circ.out",
+    duration: 0.7,
+    rotation: rotateDirection
+  });
+
+  setTimeout(() => {
+    particle.remove();
+  }, 750);
+};
+
+btn.addEventListener("mouseover", () => {
+  gsap.to(btn, { scale: 1.07, duration: 0.2 });
+});
+
+btn.addEventListener("mouseleave", () => {
+  gsap.to(btn, { scale: 1, duration: 0.2 });
+});
+
+btn.addEventListener("mousedown", () => {
+  gsap.to(btn, { scale: 1, duration: 0.05 });
+  console.log("btn clicked");
+});
+
+btn.addEventListener("mouseup", () => {
+  gsap.to(btn, { scale: 1.07, duration: 0.2, ease: "circ.out" });
+  let particleCount = Math.floor(Math.random() * (30 - 20 + 1) + 20);
+  for (let i = 0; i < particleCount; i++) {
+    createParticle();
+  }
+});
+
+gsap.to(btn, {
+  scaleX: 1,
+  duration: 1,
+  ease: "elastic.out(1, 0.3)"
+});
